59ef32a1c7a4a2c727c68fba08382a8e052af476
Dmg5th <dmgould310@gmail.com>
2021-02-17 15:46:36 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,13 @@
 export default (state = [], action) => {
-  return state;
+ switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.id)
+    case "UPVOTE-QUOTE": 
+      let newState = state.map(quote => {})
+  
+    default:
+      return state;
+  }
 }
